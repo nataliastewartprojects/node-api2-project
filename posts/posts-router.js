0511b7f34c4f46185f56d7ab663803e01e96723d
Natalia <nataliamirandac@hotmail.com>
2020-08-04 18:13:47 -0400

    GET POST BY ID - works

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -7,6 +7,7 @@ router.get("/", (req, res) => {
 
   const query = req.query;
 
+  //-->GET POSTS
   Posts.find(query) //dont forget to pass a obj here
     .then((hubs) => {
       res.status(200).json({ data: hubs, parameters: req.query });
@@ -14,6 +15,28 @@ router.get("/", (req, res) => {
     .catch((error) => {
       console.log(error);
       res.status(500).json({ error: error.message });
+    });
+});
+
+//-->>GET POST BY ID
+
+router.get("/:id", (req, res) => {
+  Posts.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch((error) => {
+      // log error to database
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
     });
 });
 
