cd823eaf205a72424f678fa916015e0760825d2a
Natalia <nataliamirandac@hotmail.com>
2020-08-04 18:33:07 -0400

    POST a post (insert a new post) - works

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -60,4 +60,24 @@ router.get("/:id/comments", (req, res) => {
     });
 });
 
+//-->>POST (ADD a Post)
+router.post("/", (req, res) => {
+  const post = req.body;
+
+  if (!post.title || !post.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post.",
+    });
+  } else {
+    try {
+      Posts.insert(post);
+      res.status(201).json(post);
+    } catch {
+      res.status(500).json({
+        error: "There was an error while saving the post to the database",
+      });
+    }
+  }
+});
+
 module.exports = router;
