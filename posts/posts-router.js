f25450a7562ad012c1f5ed4dd0605b9d7f0c8a88
Natalia <nataliamirandac@hotmail.com>
2020-08-04 18:53:12 -0400

    PUT - UPDATE and DELETE - works

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -80,4 +80,77 @@ router.post("/", (req, res) => {
   }
 });
 
+//-->>POST COMMENT (isert a new comment)
+router.post("/:id/comments", (req, res) => {
+  const id = req.params.id;
+  const comment = req.body;
+
+  if (!comment.text) {
+    res.status(400).json({ message: "no text" });
+  } else {
+    Posts.insertComment({ post_id: id, ...comment }).then((comment) => {
+      if (comment === 0) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist.",
+        });
+      } else {
+        try {
+          res.status(201).json({ id: comment.id, post_id: id, ...req.body });
+        } catch {
+          console.log(error, "500 error");
+          res.status(500).json({
+            error: "There was an error while saving the post to the database",
+          });
+        }
+      }
+    });
+  }
+});
+
+//-->> DELETE POST/:id
+
+router.delete("/:id", (req, res) => {
+  Posts.remove(req.params.id)
+    .then((post) => {
+      if (post && post > 0) {
+        res.status(200).json({ message: "The post has been deleted" });
+      } else {
+        res.status(404).json({ message: "The post could not be found" });
+      }
+    })
+    .catch((error) => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error removing the post",
+      });
+    });
+});
+
+//-->>UPDATE POST/:id - PUT
+
+router.put("/:id", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide title and contents for the post" });
+  } else {
+    Posts.update(req.params.id, req.body)
+      .then((item) => {
+        if (item) {
+          res.status(200).json({ ...req.body, id: req.params.id });
+        } else {
+          res.status(404).json({
+            message: "The post with the speciefied ID does not exist",
+          });
+        }
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ error: "The post informationd could not be modified." });
+      });
+  }
+});
+
 module.exports = router;
